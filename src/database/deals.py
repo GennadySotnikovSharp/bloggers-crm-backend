8aec9fd80c00543285fbab116d4b9f95b0126c68
Gennadii Sotnikov <gennady.sotnikov@vallettasoftware.com>
2025-06-23 13:00:39 +0200

    fix: patching of deals instead of full updates

--- src/database/deals.py
+++ src/database/deals.py
@@ -3,7 +3,6 @@ from schemas import DealData
 
 async def update_deal(deal_data: DealData):
     try: 
-        print(f"!!!!!update_deal {deal_data}")
         data = {
             "price_usd": deal_data.price_usd,
             "availability": deal_data.availability,
@@ -11,18 +10,14 @@ async def update_deal(deal_data: DealData):
             "status": deal_data.status,
             "chat_id": deal_data.chat_id
         }
-        print("!!!!!update_deal 1")
         deal = await supabase.table("deals").select("*").eq("chat_id", deal_data.chat_id).maybe_single().execute()
-        print(f"!!!!!update_deal 2 deal: {deal}")
 
         response = None
         if not deal:
-            print("!!!!!update_deal 3")
             response = await supabase.table("deals").insert(data).execute()
         else:
-            print("!!!!!update_deal 4")
-            response = await supabase.table("deals").update(data).eq("id", deal.data["id"]).execute()
-        print(f"!!!!!update_deal response: {response}")
+            patched_data = {k: v for k, v in data.items() if v is not None}
+            response = await supabase.table("deals").update(patched_data).eq("id", deal.data["id"]).execute()
         return response
     except Exception as e:
         print(f"!!!!!update_deal exception: {repr(e)}")
